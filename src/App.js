376c2c3ba01bbaef703fe7243a229c47b615988e
AxelXrest <axelxrest@gmail.com>
2024-06-13 17:22:24 +0545

    initial push

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter, Router } from 'react-router-dom';
 import { auth, db } from './config/firebase';
 import { 
     getDoc,
